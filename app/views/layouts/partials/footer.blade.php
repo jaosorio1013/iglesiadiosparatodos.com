68452ef7d720507ea49a18f836743dba757abad5
Jhonatan Osorio <jaosorio1013@gmail.com>
2014-10-08 09:49:43 -0500

    Continue cleanning views

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -8,10 +8,10 @@
 				</div>
 				<!-- /.about-us -->
 			</div>
-			<!-- /.col-md-3 -->
+			<!-- /.col-md-5 -->
 			<div class="col-md-4">
 				<div class="widget popular-posts">
-					<h5 class="subpage-title">Popular Posts</h5>
+					<h5 class="subpage-title">Posts populares</h5>
 					<ul class="recent-posts">
 						<li>
 							<img src="/assets/img/blog/recent-posts/photo (1).jpg" alt="Post Image">
@@ -20,52 +20,14 @@
 								<small>Posted July 20, 2013</small>
 							</h5>
 						</li>
-						<li>
-							<img src="/assets/img/blog/recent-posts/photo (2).jpg" alt="Post Image">
-							<h5>
-								<a href="post-1.html">Complete Guide to What’s New in Adobe Photoshop 14.1</a>
-								<small>Posted July 15, 2013</small>
-							</h5>
-						</li>
-						<li>
-							<img src="/assets/img/blog/recent-posts/photo (3).jpg" alt="Post Image">
-							<h5>
-								<a href="post-1.html">How to Integrate Lightroom Into Your Photoshop Workflow</a>
-								<small>Posted July 10, 2013</small>
-							</h5>
-						</li>
 					</ul>
 				</div>
 				<!-- /.popular-posts -->
 			</div>
-			<!-- /.col-md-3 -->
-			{{--<div class="col-md-3">
-				<div class="widget tagcloud">
-					<h5 class="subpage-title">Some Tags</h5>
-					<ul class="tagcloud-list">
-						<li><a href="#">photoshop</a>  </li>
-						<li><a href="#">html</a> </li>
-						<li><a href="#">css</a>  </li>
-						<li><a href="#">html5</a>  </li>
-						<li><a href="#">css3</a> </li>
-						<li><a href="#">illustration</a>  </li>
-						<li><a href="#">javascript</a>  </li>
-						<li><a href="#">jquery</a>  </li>
-						<li><a href="#">bootstrap 2</a> </li>
-						<li><a href="#">bootstrap 3</a>  </li>
-						<li><a href="#">php</a>  </li>
-						<li><a href="#">mysql</a> </li>
-						<li><a href="#">wordpress</a>  </li>
-						<li><a href="#">ajax</a>  </li>
-						<li><a href="#">webmatrix</a>  </li>
-					</ul>
-				</div>
-				<!-- /.tagcloud -->
-			</div>--}}
-			<!-- /.col-md-3 -->
+			<!-- /.col-md-4 -->
 			<div class="col-md-3">
 				<div class="widget flickr-photos">
-					<h5 class="subpage-title">Flickr Photos</h5>
+					<h5 class="subpage-title">Fotos en Flickr</h5>
 					<ul class="flickr-photos-list"></ul>
 				</div>
 				<!-- /.flickr-photos -->
--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -107,42 +107,6 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-md-4 team-member-wrapper">
-					<div class="team-member">
-						<div class="team-member-thumb">
-							<img src="assets/img/smiling-group/photo (2).jpg" class="img-responsive" alt="Member Image">
-							<div class="overlay"></div>
-							<ul class="member-social">
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-google-plus"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							</ul>
-						</div>
-						<div class="team-member-info">
-							<h5>Pastor 2</h5>
-							<span>Pastor</span>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4 team-member-wrapper">
-					<div class="team-member">
-						<div class="team-member-thumb">
-							<img src="assets/img/smiling-group/photo (3).jpg" class="img-responsive" alt="Member Image">
-							<div class="overlay"></div>
-							<ul class="member-social">
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-google-plus"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							</ul>
-						</div>
-						<div class="team-member-info">
-							<h5>Pastor 3</h5>
-							<span>Pastor</span>
-						</div>
-					</div>
-				</div>
 			</div>
 			<!-- /.row -->
 		</div>
